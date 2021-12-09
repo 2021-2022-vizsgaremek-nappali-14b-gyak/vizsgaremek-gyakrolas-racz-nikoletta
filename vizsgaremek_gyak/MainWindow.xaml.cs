31ef227ca50df3772bca14b78f8a6d1bfdea4e4e
sz0racnik <sz0racnik@vasvari.org>
2021-12-09 11:17:12 +0100

    eddigi

---
+++ vizsgaremek_gyak/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using vizsgaremek_gyak.Navigation;
+using vizsgaremek_gyak.Pages;
+
+namespace vizsgaremek_gyak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Statikus osztály a Navigate
+            // Eltárolja a nyitó ablakt, hogy azon tudjuk módosítani a "page"-ket
+            Navigate.mainWindow = this;
+            // Létrehozzuk a nyitó "UsuerControl" (WelcomPage)
+            WelcomePage welcomePage = new WelcomePage();
+            // Megjelnítjük a WelcomePage-t
+            Navigate.Navigation(welcomePage);
+
+
+        }
+
+
+    }
+}
---
+++ vizsgaremek_gyak/Navigation/Navigate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using vizsgaremek_gyak.Pages;
+
+namespace vizsgaremek_gyak.Navigation
+{
+    public static class Navigate
+    {
+        public static MainWindow mainWindow;
+
+        public static void Navigation(UserControl userControl)
+        {
+            mainWindow.PageContent.Children.Add(userControl);
+        }
+    }
+
+    
+}
+
